dfde56e54e67c5a9bcfad207ec1fd33708ee0ecc
Hoangduong1191 <trannamhoangduong12@gmail.com>
2017-06-04 09:36:30 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -2,7 +2,7 @@
 	var url_popup = 'http://tragopxevespaphohue.blogspot.com';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'http://tragopxevespaphohue.blogspot.com';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
